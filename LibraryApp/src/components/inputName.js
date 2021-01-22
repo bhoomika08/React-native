23482759f9f2fc2f05d12eb56a3d1a505efc642c
Bhoomika <bhoomika@vinsol.com>
2021-01-22 19:19:20 +0530

    configure insalled packages + use of absolute path

--- LibraryApp/src/components/inputName.js
+++ LibraryApp/src/components/inputName.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {Text, View, TextInput, StyleSheet} from 'react-native';
 import {setName} from 'store/actions/user.js';
