6a7dcfc19d4bb9fe8aaf100ad4c5e801271e666c
Bhoomika <bhoomika@vinsol.com>
2021-03-16 20:59:27 +0530

    pulsating animation of circle

---
+++ Animations/src/stylesheets/colors.js
@@ -0,0 +1,4 @@
+export const blue = '#0000FF';
+export const lightBlue = '#5CB3FF';
+export const lightGreen = '#90EE90';
+export const pink = '#F1948A';
---
+++ Animations/src/stylesheets/global.js
@@ -0,0 +1,22 @@
+export const flex1 = {
+  flex: 1,
+};
+export const rowFlex = {
+  flexDirection: 'row',
+};
+export const columnFlex = {
+  flexDirection: 'column',
+};
+export const verticalCenter = {
+  justifyContent: 'center',
+};
+export const horizontalCenter = {
+  alignItems: 'center',
+};
+export const center = {
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+export const borderRadius50 = {
+  borderRadius: 50,
+};
