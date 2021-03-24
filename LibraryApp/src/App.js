f6d16571975879c66c2c4e722b2c8e2438f30d68
Bhoomika <bhoomika@vinsol.com>
2021-03-24 20:50:34 +0530

    add splash screen + app icon for ios & android

--- LibraryApp/src/App.js
+++ LibraryApp/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import store from 'store';
 import {persistor} from './store';
 import LibraryApp from 'components/library-app';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 5000);
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
