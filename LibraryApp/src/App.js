e3ce9932a71f5e680a8fa6d2e062ec5b0ec99542
Bhoomika Gupta <bhoomika@vinsol.com>
2021-04-07 14:09:07 +0530

    Splash screen & App Icon for Android & IOS
    * add image to ios splash screen + change app icon img.
    * change app icon in android + add background color to splash screen

--- LibraryApp/src/App.js
+++ LibraryApp/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import store from 'store';
 import {persistor} from './store';
 import LibraryApp from 'components/library-app';
+import SplashScreen from 'react-native-splash-screen';
+
+const splashScreenTimeout = 5000;
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, splashScreenTimeout);
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
