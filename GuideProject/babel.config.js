bd961964e963c7d2ab8f317f17340f53ebaa478b
Bhoomika <bhoomika@vinsol.com>
2021-05-29 17:28:16 +0530

    temp commit

--- GuideProject/babel.config.js
+++ GuideProject/babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@src": "./src",
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@sharedComponents": "./src/components/shared",
+          "@constants": "./src/constants",
+          "@helpers": "./src/helpers",
+          "@services": "./src/services",
+          "@store": "./src/store",
+          "@actions": "./src/store/actions",
+          "@reducers": "./src/store/reducers",
+          "@stylesheets": "./src/stylesheets"
+        }
+      }
+    ]
+  ]
 };
